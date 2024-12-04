d63a9737351a4d7e31d6922c4e2e7bfaf4077bca
hiram_chavez <hiram.chavez@dataiq.com.mx>
2024-12-04 17:44:45 -0600

    feat: add tag Availibility in a day

--- src/components/ScheduleXBasic.jsx
+++ src/components/ScheduleXBasic.jsx
@@ -1,29 +1,29 @@
 import {ScheduleXCalendar, useCalendarApp} from "@schedule-x/react";
 import {createViewMonthGrid} from "@schedule-x/calendar";
 import {createEventModalPlugin} from "@schedule-x/event-modal";
-import {createDragAndDropPlugin} from "@schedule-x/drag-and-drop";
 
 export const ScheduleXBasic = () => {
+
+    const appointments = [
+        {
+            id: 1,
+            title: "Event 1",
+            start: '2024-12-05 00:00',
+            end: '2024-12-05 02:00',
+            description: 'Description of event 1',
+        }
+    ]
 
     const calendar = useCalendarApp(
         {
             locale: 'es-ES',
+            firstDayOfWeek: 0,
             views: [
                 createViewMonthGrid(),
             ],
-            events: [
-                {
-                    id: 1,
-                    title: "Event 1",
-                    start: '2024-12-05 00:00',
-                    end: '2024-12-05 02:00',
-                    description: 'Description of event 1',
-                }
-            ],
-            selectedDate: '2024-12-04',
+            events: appointments,
             plugins: [
                 createEventModalPlugin(),
-                createDragAndDropPlugin(),
             ],
         }
     );
